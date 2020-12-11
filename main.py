3ee2dcac6bcd846677b44b5edaf4128cad5e6d48
Jeff Gebhardt <jeff@gebbey.com>
2020-12-11 13:16:35 -0800

    added logging

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import logging
+from datetime import datetime
+
 import driver
 from GLOBALS import IKON_LOGIN_URL, IKON_LOGIN_USERNAME, IKON_LOGIN_PASSWORD
 
+# Set the global format for logging messages
+global_formatter = '%(asctime)s-%(levelname)s-%(message)s'
+# Write logs to a file in './logs'
+logging.basicConfig(filename=f'./logs/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log',
+                    format= global_formatter,
+                    level=logging.INFO)
+
+# Also output logs to the console
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console_formatter = logging.Formatter(global_formatter)
+console.setFormatter(console_formatter)
+logging.getLogger().addHandler(console)
+
 mountain = 'Crystal Mountain Resort'  # TODO: Update to be dynamic
 reservation_date = 'Dec 25 2020'  # TODO: Update to be dynamic
+
+logging.info(
+    f'Attempting to make a reservation at "{mountain}" on "{reservation_date}" for user "{IKON_LOGIN_USERNAME}".')
 
 # Instantiate the driver.
 reservation_driver = driver.Driver(IKON_LOGIN_URL, IKON_LOGIN_USERNAME, IKON_LOGIN_PASSWORD, reservation_date)
